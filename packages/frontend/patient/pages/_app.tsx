0e245273260453b68703b5ad921992774a1c827e
Charuka Samarakoon <charuka@protonmail.com>
2021-10-21 23:49:24 +0530

    Add theme colors

--- packages/frontend/patient/pages/_app.tsx
+++ packages/frontend/patient/pages/_app.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 import Sidebar from "@hospe/ui-core/components/sidebar";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      background: "#F5FAF8",
+      text: "#343E3B",
+      secondry: "#5AA391",
+      sub: "#777A79",
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Sidebar>
         <Component {...pageProps} />
       </Sidebar>
